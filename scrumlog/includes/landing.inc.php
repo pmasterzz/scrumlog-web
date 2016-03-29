ae9a9da6aebc72d8506e17c8663742b1fec1ceda
beestjes <kbrouwer1994@hotmail.nl>
2016-03-29 11:33:29 +0200

    toevoegen van scrumlog map

---
+++ scrumlog/includes/landing.inc.php
@@ -0,0 +1,59 @@
+<div class="top-content">
+        	
+            <div class="inner-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2 text">
+                            <h1>Rijnijssel Scrumlog</h1>
+                            <div class="description">
+                            	<p>
+	                            	Leuker kunnen we het niet maken, wel makkelijker.
+	                            	
+                            	</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 form-box">
+                        	<div class="form-top">
+                        		<div class="form-top-left">
+                        			<h3>Login</h3>
+                            		<p>Login met uw gebruikersnaam en wachtwoord</p>
+                        		</div> 
+                        		<div class="form-top-right">
+                        			<i class="fa fa-lock"></i>
+                        		</div>
+                            </div>
+                            <div class="form-bottom">
+                                <form role="form" action="" method="post" class="login-form" action="peehaapee/login.php">
+			                    	<div class="form-group">
+			                    		<label class="sr-only" for="form-username">Gebruikersnaam</label>
+                                                        <input type="text" name="form-username" placeholder="Gebruikersnaam" class="form-username form-control" id="form-username" autofocus>
+			                        </div>
+			                        <div class="form-group">
+			                        	<label class="sr-only" for="form-password">Wachtwoord</label>
+			                        	<input type="password" name="form-password" placeholder="Wachtwoord" class="form-password form-control" id="form-password">
+			                        </div>
+			                        <button type="submit" name="submit" class="btn">Log in</button>
+			                    </form>
+		                    </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+
+        <!-- Javascript -->
+        <script src="assets/js/jquery-1.11.1.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+        
+        <!--[if lt IE 10]>
+            <script src="assets/js/placeholder.js"></script>
+        <![endif]-->
