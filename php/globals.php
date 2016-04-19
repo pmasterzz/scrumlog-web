8bedbb80a2f5cf3c4467225082a7509c19c33c4b
beestjes <kbrouwer1994@hotmail.nl>
2016-04-19 09:28:06 +0200

    Welkom2016!

---
+++ php/globals.php
@@ -0,0 +1,10 @@
+<?php
+$table = array(1,2,3,4,5,6,7);
+
+//hoi
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
