edf7708fd97b183c44464a98f086fabc5b366633
Pepijn Seigers <pepijn@seigers.nl>
2016-04-19 10:05:54 +0200

    fix getAllTeachers

--- php/getAllTeachers.php
+++ php/getAllTeachers.php
@@ -1,27 +1,28 @@
 <?php
+include_once 'database.php';
 $teachersArray = getAllTeachers();
 
-function getAllTeachers()
-{
-    $url = 'http://localhost/scrumlog-web/api/api.php/api/getAllTeachers';
+// function getAllTeachers()
+// {
+//     $url = 'http://localhost/scrumlog-web/api/api.php/api/getAllTeachers';
     
-    $ch = curl_init();
+//     $ch = curl_init();
 
-    //set the url, number of POST vars, POST data
-    curl_setopt($ch,CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+//     //set the url, number of POST vars, POST data
+//     curl_setopt($ch,CURLOPT_URL, $url);
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Token: ' . $_SESSION['Token']
-    ));
+//     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//         'Token: ' . $_SESSION['Token']
+//     ));
     
-    $result = curl_exec($ch);
-    $teacher = json_decode($result, TRUE);
+//     $result = curl_exec($ch);
+//     $teacher = json_decode($result, TRUE);
    
-    return $teacher;
+//     return $teacher;
     
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
+//     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//     curl_close($ch);
     
 
-}
+// }
