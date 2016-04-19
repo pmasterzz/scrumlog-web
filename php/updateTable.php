e64ce64164e4ebcb8586df31283f191f7001ad91
beestjes <kbrouwer1994@hotmail.nl>
2016-04-19 12:28:09 +0200

    Table EDITS EN SHIT

--- php/updateTable.php
+++ php/updateTable.php
@@ -1,5 +1,10 @@
 <?php
 include_once'database.php';
+$seat = $_POST['table'];
+$students = getSpecificTable($seat);
+var_dump($students);
+
+//setTable($students);
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
